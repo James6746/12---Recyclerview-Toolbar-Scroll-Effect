e74648ebcde7fb66b4ea7050308e9c57a935c09d
James <sobirovjamshid18@gmail.com>
2022-01-18 00:34:10 +0500

    first commit

---
+++ app/src/main/java/com/example/a12_recyclerviewtoolbarscrolleffectjava/User.java
@@ -0,0 +1,13 @@
+package com.example.a12_recyclerviewtoolbarscrolleffectjava;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
